d6b8b4afbec93e449bf04c3669d08f15d6f29e0d
MinJae Lee <wjdchs0129@gnu.ac.kr>
2023-06-02 14:21:13 +0900

    update multi action

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from multiprocessing import Process
 import threading
 
 def multiprocessingCalibration(camera_selection, SIZE):
-    global STOP_ALL_PROCESSES, K, extended_K, dist, calib_R_t_with_0001
+    global global_image, STOP_ALL_PROCESSES, K, extended_K, dist, calib_R_t_with_0001
 
     while not STOP_ALL_PROCESSES:
-        K, extended_K, dist, calib_R_t_with_0001 = ImageHandler.getCalibrationMatrices(camera_selection, SIZE)
+        global_image, K, extended_K, dist, calib_R_t_with_0001 = ImageHandler.getCalibrationMatrices(camera_selection, SIZE)
 
 def multiprocessingEstimation(camera_selection):
     global thread_selection, STOP_ALL_PROCESSES, global_image, K, extended_K, dist, calib_R_t_with_0001
@@ -18,9 +18,11 @@ def multiprocessingEstimation(camera_selection):
             if success:  # 프레임을 읽는데 성공했다면
                 results, global_image, h, w, _ = ImageHandler.processImage(hands, global_image) # 랜드마크 추적결과, 이미지의 기본 정보(이미지 픽셀별 값, 가로길이, 세로길이)
 
-                # """FOR SINGLE THREAD"""
-                # K, extended_K, calib_R_t_with_0001, dist = ImageHandler.getCalibrationMatrices(camera_selection, SIZE)
-                # """DONE"""
+                """FOR SINGLE THREAD"""
+                if thread_selection == 1:
+                    print("싱글스레드 캘리브레이션")
+                    global_image, K, extended_K, calib_R_t_with_0001, dist = ImageHandler.getCalibrationMatrices(camera_selection, SIZE)
+                """DONE"""
 
                 if results.multi_hand_landmarks:  # 손(들)을 찾았다면
                     for i, hand_landmarks in enumerate(results.multi_hand_landmarks):  # 한손이면 i는 0, 양손이면 i는 0, 1
@@ -34,10 +36,6 @@ def multiprocessingEstimation(camera_selection):
                         # H2C에서 H곱해서 나온 C좌표를 2D이미지로 찍을때 어떤 픽셀에 찍어야 할지 리스트
                         # C2W에서 C곱해서 나온 W좌표를 2D이미지로 찍을때 어떤 픽셀에 찍어야 할지 리스트
                         # 두개를 리턴받습니다.
-                        
-                        
-
-
                         h2c2i_coords, w2c2i_coords = MatrixHandler.getCoordinates(K, extended_K, dist, calib_R_t_with_0001, model_points, image_points)
 
                         # 수치적, 시각적 분석
@@ -51,31 +49,33 @@ def multiprocessingEstimation(camera_selection):
                     break
 
 
-if __name__ == "__main__":
-    STOP_ALL_PROCESSES, SIZE = False, tuple(map(int, input("체스보드 사이즈 입력 (ex. 9 7) > ").split()))
-    camera_selection = int(input("카메라 선택 > "))
-    thread_selection = int(input("모드를 선택하세요(1: 싱글스레드, 2: 멀티스레드, 3: 멀티프로세싱) > "))
 
-    global_image, K, extended_K, calib_R_t_with_0001, dist = None, *ImageHandler.getCalibrationMatrices(camera_selection, SIZE)
+STOP_ALL_PROCESSES, SIZE = False, tuple(map(int, input("체스보드 사이즈 입력 (ex. 9 7) > ").split()))
+camera_selection = int(input("카메라 선택 > "))
+thread_selection = int(input("모드를 선택하세요(1: 싱글스레드, 2: 멀티스레드, 3: 멀티프로세싱) > "))
 
-    if thread_selection == 1: # 단일 스레드
-        multiprocessingEstimation(camera_selection)
-    elif thread_selection == 2: # thread 로 도전
-        t1 = threading.Thread(target=multiprocessingEstimation, args=(camera_selection, ))
-        t1.start()
-        t2 = threading.Thread(target=multiprocessingCalibration, args=(camera_selection, SIZE))
-        t2.start()
-        t1.join()
-        t2.join()
-    elif thread_selection == 3: # Process 로 도전
-        p1 = Process(target=multiprocessingEstimation, args=(camera_selection, ))
-        p1.start()
-        p2 = Process(target=multiprocessingCalibration, args=(camera_selection, SIZE))
-        p2.start()
-        p1.join()
-        p2.join()
-    else:
-        print("잘못된 선택입니다.")
+print("1차 캘리브레이션")
+global_image, K, extended_K, calib_R_t_with_0001, dist = ImageHandler.getCalibrationMatrices(camera_selection, SIZE)
+print("1차 캘리브레이션 종료")
+
+if thread_selection == 1: # 단일 스레드
+    multiprocessingEstimation(camera_selection)
+elif thread_selection == 2: # thread 로 도전
+    t1 = threading.Thread(target=multiprocessingEstimation, args=(camera_selection, ))
+    t1.start()
+    t2 = threading.Thread(target=multiprocessingCalibration, args=(camera_selection, SIZE))
+    t2.start()
+    t1.join()
+    t2.join()
+elif thread_selection == 3: # Process 로 도전
+    p1 = Process(target=multiprocessingEstimation, args=(camera_selection, ))
+    p1.start()
+    p2 = Process(target=multiprocessingCalibration, args=(camera_selection, SIZE))
+    p2.start()
+    p1.join()
+    p2.join()
+else:
+    print("잘못된 선택입니다.")
     
     
     
